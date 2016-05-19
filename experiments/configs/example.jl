68cffbca60cf9d74b21a7d3920cf531c8119f894
Alden Wright <github@fastmail.us>
2016-05-18 18:17:02 -0600

    Fixed bug in  experiments/configs/example.jl.

--- experiments/configs/example.jl
+++ experiments/configs/example.jl
@@ -1,5 +1,5 @@
-const N     = 200     # Population size
-const mu    = 1.0/N   # Mutation rate
-const ngens = 1     # Generations
-const burn_in= 200    # generations of burn_in
-const T     = 10      # Number of trials
+@everywhere N     = 200     # Population size
+@everywhere mu_list= [0.02, 0.05]   # Mutation rate list
+@everywhere ngens = 1       # Generations
+@everywhere burn_in= 200    # generations of burn_in
+@everywhere T     = 10      # Number of trials
