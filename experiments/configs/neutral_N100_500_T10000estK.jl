c616eb13a945276b745aaa42ff7c0ebe8b6f5d24
Alden Wright <github@fastmail.us>
2016-06-04 08:52:51 -0600

    Added configuration file experiments/configs/neutral_N100_500_T10000estK.jl.  Removed experiments/run_simlation.jl.

---
+++ experiments/configs/neutral_N100_500_T10000estK.jl
@@ -0,0 +1,6 @@
+const N_list   = [100, 500]   # Population size list
+const mu_list    = [1.0, 2, 4, 10.0]   # Mutation rate as a multiple of  1.0/N
+const ngens = 1       # Generations
+const burn_in= 2.0    # generations of burn_in as a multiple of N
+const T     = 10000    # Number of trials
+const slat_reps=-1 # slat_reps < 0 means estimate K
