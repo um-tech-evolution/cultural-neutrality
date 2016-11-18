cc2a14cca8124384045ddbfa0a9d3c97528dbd23
Alden Wright <github@fastmail.us>
2016-11-18 13:00:40 -0700

    Fixed bugs and added dfe_mod  for a fitness function that increases the fitness of innovations divisible by a specified number.

--- src/nearly_neutral_poplist.jl
+++ src/nearly_neutral_poplist.jl
@@ -1,15 +1,20 @@
-export fitness, nearly_neutral_poplist, dfe_deleterious,dfe_advantageous, dfe_mixed
+export fitness, nearly_neutral_poplist, dfe_deleterious,dfe_advantageous, dfe_mixed, dfe_mod
 
 using Distributions
 
 @doc """ function fitness( p::Int64, dfe::Function )
 Fitness function as saveed in dictionary fitness_table.
 """
-function fitness( p::Int64, dfe::Function, nn_select::Int64 )
+function fitness( p::Int64, dfe::Function )
   global fitness_table = Dict{Int64,Float64}()
+	global nn_select = 1
   val = get(fitness_table,p,-1.0)
   if val == -1.0   # p is not in fitness table
-    val = dfe( p, nn_select )
+		if nn_select == 1
+    	val = dfe( p )
+		else
+    	val = 1.0
+		end
     fitness_table[p] = val
   end
   val
@@ -18,10 +23,12 @@ end
 @doc """ function nearly_neutral_poplist( N::Int64, mu::Float64, ngens::Int64, dfe::Function; burn_in::Float64=1.0,
     uniform_start::Bool=false )
 Note:  dfe is "distribution of fitness effects" vector.  
+TODO:  Add n as a parameter which allows for sampling
 """
-function nearly_neutral_poplist( N::Int64, N_mu::Float64, ngens::Int64, dfe::Function; burn_in::Float64=1.0, 
-    uniform_start::Bool=false, nn_select::Int64=1, combine::Bool=true )
+function nearly_neutral_poplist( N::Int64, N_mu::Float64, ngens::Int64, dfe::Function; burn_in::Float64=2.0, 
+    uniform_start::Bool=false, nnselect::Int64=1, combine::Bool=true )
   int_burn_in = Int(round(N*burn_in))
+	global nn_select = 1
   mu = N_mu/N
   if uniform_start  # All allele values start with the same value.  Start with a selective sweep.
     poplist= Population[ Int64[1 for i = 1:N] ]
@@ -30,7 +37,7 @@ function nearly_neutral_poplist( N::Int64, N_mu::Float64, ngens::Int64, dfe::Fun
   else
     poplist= Population[ collect(1:N) ]
     for i = 1:N
-      fitness(i, dfe, 0 )  # set fitness of i to be 1.0
+      fitness(i, dfe )  # set fitness of i to be 1.0
     end
     new_id = N+1
   end
@@ -41,7 +48,7 @@ function nearly_neutral_poplist( N::Int64, N_mu::Float64, ngens::Int64, dfe::Fun
     for i in 1:N
       if rand() < mu
         poplist[g-1][i] = new_id
-        fitness( new_id, dfe, nn_select )  # Set fitness of new_id
+        fitness( new_id, dfe )  # Set fitness of new_id
         new_id += 1
       end
     end
@@ -58,20 +65,22 @@ function nearly_neutral_poplist( N::Int64, N_mu::Float64, ngens::Int64, dfe::Fun
   end
 end
 
-function dfe_deleterious( x::Int64, nn_select::Int64; alpha::Float64=0.2, theta::Float64=0.5 )
+function dfe_deleterious( x::Int64; alpha::Float64=0.2, theta::Float64=0.5 )
   global dist_deleterious
+	global nn_select
   if !isdefined(:dist_deleterious)
     dist_deleterious = Distributions.Gamma(alpha,theta)
   end
   if nn_select == 1
-    return 1.0-rand(dist_deleterious)
+    return max(0.1,1.0-rand(dist_deleterious))
   else
     return 1.0  # flat fitness for no selection
   end
 end
   
-function dfe_advantageous( N::Int64, nn_select::Int64; alpha::Float64=1.0, theta::Float64=0.01 )
+function dfe_advantageous( x::Int64; alpha::Float64=1.0, theta::Float64=0.01 )
   global dist_advantageous
+	global nn_select
   if !isdefined(:dist_advantageous)
     dist_advantageous = Distributions.Gamma(alpha,theta)
   end
@@ -82,18 +91,42 @@ function dfe_advantageous( N::Int64, nn_select::Int64; alpha::Float64=1.0, theta
   end
 end
 
-function dfe_mixed( N::Int64, nn_select::Int64; adv_probability::Float64=0.2, alpha_disadv::Float64=0.2, alpha_adv::Float64=1.0, theta_disadv::Float64=1.0, theta_adv::Float64=0.01 )
+function dfe_mixed( x::Int64; adv_probability::Float64=0.2, alpha_disadv::Float64=0.2, alpha_adv::Float64=1.0, theta_disadv::Float64=1.0, theta_adv::Float64=0.01 )
   global dist_deleterious
   if !isdefined(:dist_deleterious)
     dist_deleterious = Distributions.Gamma(alpha_disadv,theta_disadv)
   end
   global dist_advantageous
+	global nn_select
   if !isdefined(:dist_advantageous)
     dist_advantageous = Distributions.Gamma(alpha_adv,theta_adv)
   end
   if nn_select == 1
-    rand() < adv_probability ? 1.0+rand(dist_advantageous) : 1.0-rand(dist_disadvantageous)
+    rand() < adv_probability ? 1.0+rand(dist_advantageous) : max(0.01,1.0-rand(dist_deleterious))
   else
-    return fill(1.0,N)  # flat fitness for no selection
+    return 1.0  # flat fitness for no selection
   end
 end
+
+function dfe_mod( x::Int64; modulus::Int64=5, fit_inc::Float64=1.1 )
+	global nn_select
+  if nn_select == 1
+		if x % modulus == 0
+			return fit_inc
+		else
+			return 1.0
+		end
+  else
+    return 1.0  # flat fitness for no selection
+  end
+end
+
+#=  Tests
+nn_select = 1
+for i = 1:10 println(fitness(i,dfe_mod)) end
+for i = 1:10 println(fitness(i,dfe_mixed)) end
+N=10; N_mu =2.0; ngens = 3;
+nearly_neutral_poplist(N,N_mu,ngens,dfe_mod)
+Fails:
+for i = 1:10 println(fitness(i,x->dfe_mod(x,fit_inc=2.0),1)) end
+=#
