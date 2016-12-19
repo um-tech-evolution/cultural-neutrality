9c3c1e9ef5c231edfb452336bd5e5ecac19f4880
Alden Wright <github@fastmail.us>
2016-12-19 11:05:04 -0700

    Minor changes to experiments/multiple_slatkin.jl.

--- experiments/multiple_slatkin.jl
+++ experiments/multiple_slatkin.jl
@@ -4,14 +4,14 @@ Run multiple Slatkin (or Watterson) tests on successive generations of a
 Sample run:
 >  julia -p 8
 julia> include("multiple_slatkin.jl")   # this file
-julia> rtn = setup_neutral_params(50,50,2.0,1)
+julia> @everywhere rtn = setup_neutral_params(50,50,2.0,1)
 julia> run_trials_neutral(200,rtn)
 =#
 @everywhere include("../src/NeutralCulturalEvolution.jl")
 
 using Dates
 
-Btbl = Array{Float64,2}()
+@everywhere Btbl = Array{Float64,2}()
 
 @everywhere type neutral_trial_result
   n::Int64
@@ -39,9 +39,9 @@ end
   tr = neutral_trial_result(n,N,N_mu,0,ngens,burn_in,Vector{Int64}())
 end
 
-@everywhere function setup_stewart_params( n::Int64, N::Int64, K::Int64, ngens::Int64=1; burn_in::Float64=2.0 )
+@everywhere function setup_stewart_params(  N::Int64, K::Int64, ngens::Int64=1; burn_in::Float64=2.0 )
   global Btbl = BT(K,N)
-  tr = neutral_trial_result(n,N,0.0,K,ngens,burn_in,Vector{Int64}())
+  tr = neutral_trial_result(0,N,0.0,K,ngens,burn_in,Vector{Int64}())
 end
 
 icurrent_dir = pwd()
@@ -49,25 +49,35 @@ data_string = "../data/"*Dates.format(now(),"mm_dd_yy")*"/"
 try mkdir(data_string) catch end   # create today's directory with no error if it already exists
 println("data string: ",data_string)
 
-@everywhere function slat_montecarlo( pop::Vector{Int64}, nreps::Int64=100000 )
-  ewens_montecarlo( Int32(nreps), pop_counts32(pop) ).probability
+@everywhere function slat_montecarlo( pop_counts::Vector{Int64}, nreps::Int64=100000 )
+  ewens_montecarlo( nreps, sort(pop_counts,rev=true) ).probability
 end
 
 @doc """ function run_trial_neutral( ntr::neutral_trial_result )
-Run a neutral neutral population evolution, then returns the Slatkin probability.
+Run a neutral neutral population evolution, then returns either the 
+    Slatkin probability( if ntr.ngens=1, or the average of the Slatkin probabilities
+    of ntr.ngens successive populations
 n = sample size
 N = popsize
 N_mu = mutation rate per population, i. e.,  mu*N.  Thus, mu = N_mu/N.
 """
 @everywhere function run_trial_neutral( ntr::neutral_trial_result)
-  if ntr.n < ntr.N
-    s_m =  slat_montecarlo( sample_population(neutral_poplist(ntr.N, ntr.N_mu, ntr.ngens, burn_in=ntr.burn_in, combine=false )[ntr.ngens],ntr.n))
-  else
-    s_m = slat_montecarlo(neutral_poplist(ntr.N, ntr.N_mu, ntr.ngens, burn_in=ntr.burn_in, combine=false )[ntr.ngens])
+  if ntr.ngens == 1
+    pop = neutral_poplist(ntr.N, ntr.N_mu, ntr.ngens, burn_in=ntr.burn_in, combine=false )[ntr.ngens]
+    if ntr.n < ntr.N
+      s_m =  slat_montecarlo( pop_counts64( sample_population( pop, ntr.n )))
+    else
+      s_m =  slat_montecarlo( pop_counts64( pop ))
+    end
+  else # ntr.ngens > 1
+    poplist = neutral_poplist(ntr.N, ntr.N_mu, ntr.ngens, burn_in=ntr.burn_in, combine=false )[1:ntr.ngens]
+    if ntr.n < ntr.N
+      s_m_list = map( x->slat_montecarlo( pop_counts64(sample_population(x,ntr.n))), poplist )
+    else
+      s_m_list = map( x->slat_montecarlo( pop_counts64(x)),poplist )
+    end
+    s_m = sum(s_m_list)/ntr.ngens
   end
-  #filename = "$(data_string)neutral_N:$(N)_N_mu:$(N_mu)_ngens:$(ngens).png"
-  #push!(ntr.sprobs, s_m)
-  #ntr
   s_m
 end
 
@@ -77,7 +87,7 @@ n = sample size
 N = popsize
 """
 @everywhere function run_trial_stewart( ntr::neutral_trial_result)
-  s_m = slat_montecarlo(Rsample(ntr.N, ntr.K, Btbl ))
+  s_m = slat_montecarlo(sort(Rsample(ntr.N, ntr.K, Btbl ),rev=true))
 end
 
 @doc """ function writeheader(stream::IOStream, t_result::trial_result)
@@ -103,7 +113,11 @@ Write a single-column CSV file where the column is the Slatkin p-values returned
 The parameters specified by tr are written as comments preceding the Slatkin p-values.
 """
 function write_slat_results( tr::neutral_trial_result,  filename::AbstractString="" )
-  filename = "$(data_string)neutral_slatkin_N:$(tr.N)_N_mu:$(tr.N_mu)_ngens:$(tr.ngens).csv"
+  if tr.n == 0 && tr.N_mu == 0.0
+    filename = "$(data_string)neutral_slatkin_N:$(tr.N)_N_mu:$(tr.N_mu)_ngens:$(tr.ngens).csv"
+  else
+    filename = "$(data_string)neutral_inf_alleles_N:$(tr.N)_N_mu:$(tr.N_mu)_ngens:$(tr.ngens).csv"
+  end
   stream = open(filename, "w")
   writeheader( stream, tr )
   #write_sprobs( stream, tr.sprobs )
