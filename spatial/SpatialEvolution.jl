1fe794ad8d75ede6ea8386ec6f93108b52f9a7ee
Alden Wright <github@fastmail.us>
2017-03-25 13:41:49 -0600

    Removed spatial files since they have been moved to a separate repositiory.

--- spatial/SpatialEvolution.jl
+++
@@ -1,8 +0,0 @@
-module SpatialEvolution
-include("../spatial/types.jl")
-include("../spatial/propsel.jl")
-include("../spatial/spatial.jl")
-include("../spatial/run_spatial.jl")
-end
-
-using SpatialEvolution
